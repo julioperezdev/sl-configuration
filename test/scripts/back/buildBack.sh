a981b85e2ff3596a947b278ccc72e1e573f52195
Julio Perez <julioperez.dev@gmail.com>
2023-12-03 16:41:30 -0300

    update route of backemd build on test and prod environment

--- test/scripts/back/buildBack.sh
+++ test/scripts/back/buildBack.sh
@@ -1,4 +1,4 @@
-mvn -f ./../../../sl-api/pom.xml clean
-mvn -f ./../../../sl-api/pom.xml compile
-mvn -f ./../../../sl-api/pom.xml package
-docker build -t sl-api-test:0.1 ./../../../sl-api/
+mvn -f ./../../../../sl-api/pom.xml clean
+mvn -f ./../../../../sl-api/pom.xml compile
+mvn -f ./../../../../sl-api/pom.xml package
+docker build -t sl-api-test:0.1 ./../../../../sl-api/
